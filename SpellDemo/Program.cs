322dc99baa4be81564ad6d1bec4a35354224102e
vinhphuctadang <vinhphuctadang@gmail.com>
2021-03-29 22:42:58 +0700

    add code for scheme, uncompleted

--- SpellDemo/Program.cs
+++ SpellDemo/Program.cs
@@ -11,8 +11,15 @@ namespace CustomSpell
         //Load a frequency dictionary or create a frequency dictionary from a text corpus
         public static void Main(string[] args)
         {
-            var path = AppDomain.CurrentDomain.BaseDirectory + @"../../../all-suggests-cleaned.txt";
-            // var path = AppDomain.CurrentDomain.BaseDirectory + @"../../../small-suggests.txt";
+
+            // int a = 5;
+            // byte[] b = BitConverter.GetBytes(a);
+            // Console.WriteLine(BitConverter.ToString(b));
+
+            // byte[] c = BitConverter.GetBytes(42);
+            // Console.WriteLine(BitConverter.ToInt32(c));
+            // var path = AppDomain.CurrentDomain.BaseDirectory + @"../../../all-suggests-cleaned.txt";
+            var path = AppDomain.CurrentDomain.BaseDirectory + @"../../../small-suggests.txt";
             Console.WriteLine("Creating trie for searching...");
             long memSize = GC.GetTotalMemory(true);
             Stopwatch stopWatch = new Stopwatch();
@@ -52,7 +59,7 @@ namespace CustomSpell
             long memDeltaForStoringValues = GC.GetTotalMemory(true) - memSize;
             Console.WriteLine("Memory for storing value: " + memDeltaForStoringValues + ". Going to add to trie");
 
-            var trie = new UkkonenTrieWord<int>(1, wordToIndex);
+            var trie = new UkkonenTrieWord(1, wordToIndex);
             int value = 0;
             foreach(var phrase in phraseList){
                 trie.Add(phrase, value++);
@@ -61,43 +68,44 @@ namespace CustomSpell
             long memDelta = GC.GetTotalMemory(true) - memSize;
             Console.WriteLine("Done in " + stopWatch.Elapsed.TotalMilliseconds.ToString("0.0") + "ms "
                 + (memDelta / 1024 / 1024.0).ToString("N0") + " MB. Token count: " + wordToIndex.Count);
-        
-            // build spell checker
-            var spellChecker = new SymSpell(wordToIndex.Count, 2);
-            foreach(var entry in wordCount) {
-                spellChecker.CreateDictionaryEntry(entry.Key, entry.Value);
-            }
 
-            while(true) {
-                Console.WriteLine("Please input string:");
-                var s = Console.ReadLine();
-                var normalized = s.ToLower();
+            trie.Save();
+            // // build spell checker
+            // var spellChecker = new SymSpell(wordToIndex.Count, 2);
+            // foreach(var entry in wordCount) {
+            //     spellChecker.CreateDictionaryEntry(entry.Key, entry.Value);
+            // }
 
-                if (normalized == "exit") {
-                    // terminate on input=="exit"
-                    return;
-                }
+            // while(true) {
+            //     Console.WriteLine("Please input string:");
+            //     var s = Console.ReadLine();
+            //     var normalized = s.ToLower();
 
-                var suggestions = spellChecker.LookupCompound(normalized);
-                var suggestion = suggestions[0].term;
-                foreach(var tmp in suggestions) {
-                    Console.WriteLine("Correct spell may be: " + tmp.term);
-                }
-                if (suggestion != normalized) {
-                    Console.WriteLine("Did you mean: " + suggestion);
-                }
+            //     if (normalized == "exit") {
+            //         // terminate on input=="exit"
+            //         return;
+            //     }
+
+            //     var suggestions = spellChecker.LookupCompound(normalized);
+            //     var suggestion = suggestions[0].term;
+            //     foreach(var tmp in suggestions) {
+            //         Console.WriteLine("Correct spell may be: " + tmp.term);
+            //     }
+            //     if (suggestion != normalized) {
+            //         Console.WriteLine("Did you mean: " + suggestion);
+            //     }
                 
-                stopWatch = new Stopwatch();
-                stopWatch.Start();
-                var result = trie.Retrieve(suggestion);
+            //     stopWatch = new Stopwatch();
+            //     stopWatch.Start();
+            //     var result = trie.Retrieve(suggestion);
 
-                stopWatch.Stop();
-                Console.WriteLine("Done searching in " + stopWatch.Elapsed.TotalMilliseconds.ToString("0.0") + "ms ");
+            //     stopWatch.Stop();
+            //     Console.WriteLine("Done searching in " + stopWatch.Elapsed.TotalMilliseconds.ToString("0.0") + "ms ");
             
-                foreach(var res in result) {
-                    Console.WriteLine("Suggestions: " + phraseList[res]);
-                }
-            }
+            //     foreach(var res in result) {
+            //         Console.WriteLine("Suggestions: " + phraseList[res]);
+            //     }
+            // }
         }
     }
 }
