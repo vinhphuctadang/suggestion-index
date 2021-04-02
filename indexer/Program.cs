496e82d9fa6f75bdd8917b1e96a200096953a050
vinhphuctadang <vinhphuctadang@gmail.com>
2021-04-02 21:48:53 +0700

    upgrade search soluion

--- indexer/Program.cs
+++ indexer/Program.cs
@@ -22,13 +22,14 @@ namespace indexer
         }
         static void Main(string[] args)
         {
-            var path = AppDomain.CurrentDomain.BaseDirectory + "all-suggests-cleaned.txt";
-            var indexerPath = AppDomain.CurrentDomain.BaseDirectory + "all-suggests-cleaned.index";
-            // var path = AppDomain.CurrentDomain.BaseDirectory + "small-suggests.txt";
-            // var indexerPath = AppDomain.CurrentDomain.BaseDirectory + "small-suggests.index";
-            var counter = new Dictionary<string, int>();
+            // var path = AppDomain.CurrentDomain.BaseDirectory + "all-suggests-cleaned.txt";
+            // var indexerPath = AppDomain.CurrentDomain.BaseDirectory + "all-suggests-cleaned.index";
+            var path = AppDomain.CurrentDomain.BaseDirectory + "small-suggests.txt";
+            var indexerPath = AppDomain.CurrentDomain.BaseDirectory + "small-suggests.index";
+            var dict = new Dictionary<string, int>();
+            var frequency = new Dictionary<string, int>();
             var inverter = new Dictionary<int, HashSet<int>>();
-            var stringList = new List<string>();
+            var documents = new List<string>();
             
             // try indexing by using invert index
             var count = 0;
@@ -42,20 +43,21 @@ namespace indexer
             var trie = new PatriciaSuffixTrie<string>(1);
             
             using (var sr = new StreamReader(path)) {
-                string s;
-
+                string s = null;
                 while ((s = sr.ReadLine()) != null) {
-
-                    stringList.Add(s);
-
+                    documents.Add(s);
                     foreach(var word in s.Split(' ')) {
                         int tmp;
-                        if (!counter.TryGetValue(word, out tmp)) {
-                            counter[word] = ++count;         
+                        if (!dict.TryGetValue(word, out tmp)) {
+                            dict[word] = ++count;
+                            frequency[word] = 1;
+                        }
+                        else {
+                            frequency[word]++;
                         }
 
                         HashSet<int> tmpStringIndexes;
-                        if (!inverter.TryGetValue(counter[word], out tmpStringIndexes)) {
+                        if (!inverter.TryGetValue(dict[word], out tmpStringIndexes)) {
                             tmpStringIndexes = new HashSet<int>();
                             inverter[count] = tmpStringIndexes;
                         }
@@ -64,93 +66,127 @@ namespace indexer
                     stringIndex ++;
                 }
             }
-            Console.WriteLine("Adding words to trie ...");
-            // add word to trie
+
+            Console.WriteLine("Adding to completion dict...");
             memSize = GC.GetTotalMemory(true);
-            foreach(var entry in counter) {
+            foreach(var entry in dict) {
                 trie.Add(entry.Key, entry.Key);
             }
+            Console.WriteLine("Add to completion dict: " + ((GC.GetTotalMemory(true)-memSize) / 1024 / 1024.0).ToString("N0") + " MB. Token count: " + dict.Count);
+            
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine("Adding to symSpell for fast spellCheck");
+            memSize = GC.GetTotalMemory(true);
+            // dictionary for symspell
+            var spellChecker = new SymSpell(dict.Count, 2);
+            foreach(var entry in frequency) {
+                spellChecker.CreateDictionaryEntry(entry.Key, entry.Value);
+            }
 
-            Console.WriteLine("Add to trie: " + ((GC.GetTotalMemory(true)-memSize) / 1024 / 1024.0).ToString("N0") + " MB. Token count: " + counter.Count);
-            stopWatch.Stop();
-            long memDeltaForStoringValues = GC.GetTotalMemory(true) - memSize;
-            Console.WriteLine("Everything done in " + stopWatch.Elapsed.TotalMilliseconds.ToString("0.0") + "ms "
-                + (memDeltaForStoringValues / 1024 / 1024.0).ToString("N0") + " MB. Token count: " + counter.Count);
+            Console.WriteLine("Spell dictionary constructed. " + ((GC.GetTotalMemory(true)-memSize) / 1024 / 1024.0).ToString("N0") + "MB, " + stopWatch.Elapsed.TotalMilliseconds.ToString("0.0") + "ms. Tokens:" + frequency.Count);
 
             stopWatch = new Stopwatch();
             stopWatch.Start();
-            var result = Search("bitcurious", trie, inverter, counter, stringList, 10);
+            Console.WriteLine("Searching ...");
+            var hits = Search("ant man", trie, spellChecker, inverter, dict, documents, 10);
             stopWatch.Stop();
-
-            Console.WriteLine("Search done in " + stopWatch.Elapsed.TotalMilliseconds.ToString("0.0") + "ms, hits: " + result.Count);
-            
+            var timeEllapsed = stopWatch.Elapsed.TotalMilliseconds.ToString("0.0");
+            foreach(var hit in hits) {
+                Console.WriteLine("--> " + hit);
+            }
+            Console.WriteLine("Searching done." + timeEllapsed + "ms. Hits:" + hits.Length);
         }
 
-        static Dictionary<int, SearchResult> Search(string query, PatriciaSuffixTrie<string> trie, Dictionary<int, HashSet<int>> inverter, Dictionary<string, int> counter, List<string> stringList, int limit) {
-            var indexToResult = new Dictionary<int, SearchResult>();
-            var tokens = query.ToLower().Split(' ').ToList();
-            
-            bool forceStop = false;
-            for(int i = 0; i < tokens.Count; ++i) {
-                var word = tokens[i];
-                int wordIndex = 0;
-                // not hit? try to fix that
-                if (!counter.TryGetValue(word, out wordIndex)) {
-                    // Fix the word
-                    foreach(var suggest in trie.Retrieve(word)) {
-                        tokens[0] = suggest;
-                        --i;
-                        break;
-                    }
+        static SearchResult[] Search(
+            // input query
+            string query, 
+            // trie for prefix/infix matching
+            PatriciaSuffixTrie<string> trie,
+            SymSpell symSpell,
+            // inverted index
+            Dictionary<int, HashSet<int>> inverter, 
+            // word -> its order
+            Dictionary<string, int> dict,
+            // collection of documents
+            List<string> documents,
+            // limit
+            int limit
+        ) {
+            var aggregated = new Dictionary<int, SearchResult>();
+            var tokens = new LinkedList<string>();
+            foreach(var word in query.ToLower().Split(' ')) {
+                tokens.AddLast(word);
+            }
+
+            while (tokens.Count > 0) {
+                
+                // pop_front the queue
+                var word = tokens.First.Value;
+                tokens.RemoveFirst();
+                // pipeline:
+                // 1. find exact matches first
+                int tmp;
+                if (dict.TryGetValue(word, out tmp)) {
+                    var docs = inverter[tmp];
+                    foreach(var doc in docs) {
+                        SearchResult tempList;
+                        // add to aggregated result
+                        if (!aggregated.TryGetValue(doc, out tempList)) {
+                            tempList = new SearchResult(word, 1);
+                        }
+                        else {
+                            tempList.score ++;
+                        }
+                    }    
                     continue;
                 }
 
-                // Console.WriteLine("Finding word = " + word + ", Hits: " + String.Join(", ", inverter[wordIndex]));
-                // hits
-                foreach(var index in inverter[wordIndex]) {
-                    SearchResult tmp;
-
-                    // skip, use the top most
-                    // if (indexToResult.Count >= limit) {
-                    //     forceStop = true;
-                    //     break;
-                    // }
-
-                    if (!indexToResult.TryGetValue(index, out tmp)) {
-                        tmp = new SearchResult(stringList[index], (tokens.Count - i)*(tokens.Count - i));
-                        indexToResult[index] = tmp;
+                // if no exact match then search for prefix suggestions (for prefix <= 3)
+                if (word.Length <= 3) { // find prefix matches
+                    string suggestion = null;
+                    // take 1 suggestion first
+                    foreach(var suggest in trie.Retrieve(word)) {
+                        suggestion = suggest;
+                        break;
                     }
-                    else {
-                        tmp.score += (tokens.Count - i)*(tokens.Count - i);
+                    if (suggestion != null) {
+                        // push_front
+                        tokens.AddFirst(suggestion);
+                        continue;
                     }
                 }
-
-                if (forceStop) {
-                    break;
+                
+                // if no prefix suggestion found then correct spelling
+                var lookupResult = symSpell.LookupCompound(word)[0].term.Split(' ');
+                for(int i = lookupResult.Length - 1; i >= 0; --i) {
+                    tokens.AddFirst(lookupResult[i]);
                 }
             }
-            return indexToResult; // indexToResult.Values.OrderBy(x => x.score).ToArray();
+
+            // then sort??
+            return aggregated.Values.ToArray();
         }
 
-        static long SaveIndex(string indexerPath, Dictionary<int, HashSet<int>> inverter, Dictionary<string, int> counter, List<string> stringList) {
+        static long SaveIndex(string indexerPath, Dictionary<int, HashSet<int>> inverter, Dictionary<string, int> dict, List<string> documents) {
 
             long fileSize = 0;
             using (var fs = new FileStream(indexerPath, FileMode.Create))
             using (var sw = new BinaryWriter(fs, Encoding.UTF8)) {
 
-                foreach(var entry in counter) {
+                foreach(var entry in dict) {
                     sw.Write(BitConverter.GetBytes(entry.Key.Length));
                     sw.Write(Encoding.ASCII.GetBytes(entry.Key));
                     
                     fileSize += 4 + entry.Key.Length; // 4 + wordLength bytes
                 }
 
-                foreach(var phrase in stringList){
+                foreach(var phrase in documents){
                     var tokens = phrase.Split(' ');
                     sw.Write(BitConverter.GetBytes(tokens.Length));
                     fileSize += (tokens.Length + 1) * 4; // 4 + tokens.Length * 4
                     foreach(var token in tokens) {
-                        var tokenId = counter[token];
+                        var tokenId = dict[token];
                         sw.Write(BitConverter.GetBytes(tokenId));
                     }
                 }
@@ -166,5 +202,38 @@ namespace indexer
             }
             return fileSize;
         }
+
+
+        static Dictionary<string, List<int>> GetBigramIndex(Dictionary<string, int> dict){
+            var bigramIndex = new Dictionary<string, List<int>>();
+            foreach(var entry in dict) {
+                var word = entry.Key;
+                var wordIndex = entry.Value;
+                var n = word.Length;
+                for(int i = -1; i<n; ++i) {
+                    string s = null;
+                    if (i < 0) {
+                        s = "$" + word[0];
+                    }
+                    else if (i == n - 1) {
+                        s = word[i] + "$";
+                    }
+                    else {
+                        s = word[i].ToString() + word[i+1].ToString();
+                    }
+
+                    List<int> tmp;
+                    if (!bigramIndex.TryGetValue(s, out tmp)) {
+                        tmp = new List<int>();
+                        bigramIndex[s] = tmp;
+                    }
+                    tmp.Add(wordIndex);
+                }
+            }
+
+            return bigramIndex;
+        }
     }
 }
+
+// https://towardsdatascience.com/the-pruning-radix-trie-a-radix-trie-on-steroids-412807f77abc
